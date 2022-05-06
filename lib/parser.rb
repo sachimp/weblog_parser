dbef525850fab3f98a013c69a8a3bac0bd159168
sachimp <sachingandhi@live.com>
2022-05-06 18:03:42 +0100

    read the file, sort into array of pairs of strings (page visit, ip)

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,6 +1,17 @@
 # frozen_string_literal: true
 
+# specify the file path
 file_name = File.join(__dir__, 'webserver.log')
+
+# open the log to read
 log = File.open(file_name, 'r') do |f|
-  p f.read
+  f.read
+end
+
+# split the info along the spaces and make it into an array
+array = log.to_s.split
+
+#sort the array of strings into pairs (["page visit", "ip address"])
+pairs = array.each_slice(2) do |string|
+  p string
 end
