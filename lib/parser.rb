31f22fc25e4e3750016fd2bb21c71d41bc940dc2
sachimp <sachingandhi@live.com>
2022-05-09 18:07:10 +0100

    test tweaking

--- lib/parser.rb
+++ lib/parser.rb
@@ -52,6 +52,8 @@ class Parser
   end
 end
 
+# show the output!
+
 test = Parser.new(LOG)
 test.all_page_views
 puts ''
--- test/challenge_test.rb
+++ test/challenge_test.rb
@@ -5,13 +5,13 @@ require_relative '../lib/parser'
 
 class ParserTest < Minitest::Test
   describe Parser do
-    describe "#all_page_views" do
+    describe '#all_page_views' do
       it 'returns an array' do
         parser = Parser.new(@file)
         assert_kind_of Array, parser.all_page_views
       end
     end
-    describe "#unique_page_views" do
+    describe '#unique_page_views' do
       it 'returns an array' do
         parser = Parser.new(@file)
         assert_kind_of Array, parser.unique_page_views
