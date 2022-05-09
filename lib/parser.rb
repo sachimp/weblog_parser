9da5bf80fd725b78d17cc76f3727a47ac04ab95f
sachimp <sachingandhi@live.com>
2022-05-09 15:25:43 +0100

    remade the class after disastrous git reset fiasco

--- lib/parser.rb
+++ lib/parser.rb
@@ -10,38 +10,49 @@ LOG = File.open(file_name, 'r') do |f|
   f.read
 end
 
-# create the methods, which call on a display method at the end to return the list appropriately
-
-def page_views(log)
-  array = log.to_s.split
-  page_names = []
-  array.each_slice(2) do |string_pair|
-    page_names << string_pair[0]
+# service object called with a file to parse, methods complete the appropriate tasks
+class Parser
+  def initialize(file)
+    @file = file
   end
-  display(page_names.tally.sort_by { |_k, v| -v })
+
+  def all_page_views
+    array = log_to_array(@file)
+    page_names = array.each_slice(2).map do |string_pair|
+      string_pair[0]
+    end
+    display(page_names.tally.sort_by { |_k, v| -v })
+  end
+
+  def unique_page_views
+    array = log_to_array(@file)
+    unique = find_unique_pairs(array)
+    pages = unique.tally.keys.map do |pair|
+      pair[0]
+    end
+    display(pages.tally.sort_by { |_k, v| -v })
+  end
+
+  private
+
+  def display(array)
+    array.map.with_index(1) do |pair, index|
+      puts "#{index}. #{pair[0]}, #{pair[1]} views"
+    end
+  end
+
+  def log_to_array(file)
+    array = file.to_s.split
+  end
+
+  def find_unique_pairs(array)
+    array.each_slice(2).map.uniq do |string_pair|
+      string_pair
+    end
+  end
 end
 
-def unique_page_views(log)
-  array = log.to_s.split
-  unique = array.each_slice(2).map.uniq do |string_pair|
-    string_pair
-  end
-  pages = unique.tally.keys.map do |pair|
-    pair[0]
-  end
-  display(pages.tally.sort_by { |_k, v| -v })
-end
-
-# display method for a nicer list view in the terminal
-# (without this it would just be an array of arrays)
-
-def display(array)
-  array.map.with_index(1) do |pair, index|
-    puts "#{index}. #{pair[0]}, #{pair[1]} views"
-  end
-end
-
-# call the methods to see the results!
-
-page_views(LOG)
-unique_page_views(LOG)
+test = Parser.new(LOG)
+test.all_page_views
+puts ''
+test.unique_page_views
---
+++ spec/parser_spec.rb
@@ -0,0 +1,8 @@
+require "parser"
+
+
+describe 'Parser' do
+  it 'receives a log as an argument' do
+    expect(Parser).to be_an_instance_of(Parser)
+  end
+end
